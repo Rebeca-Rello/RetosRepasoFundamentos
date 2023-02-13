ceef582bd4f8699788373081e80a5b75c82f0691
Rebeca Rello <rebeca.rello@gmail.com>
2023-02-13 11:47:48 +0100

    retosMobile

--- retos1.js/mobileProject/mobileTest.ts
+++ retos1.js/mobileProject/mobileTest.ts
@@ -4,4 +4,21 @@ import { mobile } from "./mobile";
 
 
 let mobile1 = new mobile ("Note", "Huawei", "i20", "black",1000);
+let mobile2 = new mobile ("Notebook", "Samsung", "40", "pink",1500);
+let mobile3 = new mobile ("Plus", "iPhone", "i15", "silver",2000);
+
+let myMobiles=[mobile1, mobile2,mobile3];
+
+for (let i=0; i<myMobiles.length;i++ ) {
+
+    
+    myMobiles.push.toString()
+    console.log(myMobiles)    
+ }
+ 
+toString()
+
+
+
+
  
