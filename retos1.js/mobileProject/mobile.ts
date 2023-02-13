62fb56422dbc4980a7bf3cbb0e25e9db41229285
Rebeca Rello <rebeca.rello@gmail.com>
2023-02-13 11:55:23 +0100

    ultimos cambios mobile

--- retos1.js/mobileProject/mobile.ts
+++ retos1.js/mobileProject/mobile.ts
@@ -82,7 +82,7 @@ public toString():string{
 
        
     return "The characteristics of the mobile name are:" + "\n" + "Name:" + this.name + "\n" + "Trademark:" +
-     this.trademark + "\n "+ "model:" + this.model  + "\n" + "Color:" + this.color + "\n" + "Price:" + this.price
+    this.trademark + "\n "+ "model:" + this.model  + "\n" + "Color:" + this.color + "\n" + "Price:" + this.price
 
 
   
--- retos1.js/mobileProject/mobileTest.ts
+++ retos1.js/mobileProject/mobileTest.ts
@@ -1,11 +1,15 @@
 
 
-import { mobile } from "./mobile";
+import { mobile} from "./mobile";
 
 
 let mobile1 = new mobile ("Note", "Huawei", "i20", "black",1000);
 let mobile2 = new mobile ("Notebook", "Samsung", "40", "pink",1500);
 let mobile3 = new mobile ("Plus", "iPhone", "i15", "silver",2000);
+
+
+mobile1.toString()
+console.log(mobile1.toString());
 
 let myMobiles=[mobile1, mobile2,mobile3];
 
